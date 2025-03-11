fb545f167ce44126407aac1a4499f965b6a454fc
Vighnesh Tathare <v42069@yahoo.com>
2025-03-11 23:44:45 +0530

    add

--- src/Collectors/Main.java
+++ src/Collectors/Main.java
@@ -1,10 +1,14 @@
 package Collectors;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 	
@@ -12,6 +16,15 @@ public class Main {
 		
 		Path p=Paths.get("/Users/BaBa/workspace-spring-tool-suite-4-4.26.0.RELEASE/Java_8_Part_2/src/Collectors/EmployeeData.txt");
 		System.out.println(p.getFileName());
+		
+		//to read data 
+		try (Stream<String> lines = Files.lines(p)){
+			lines.flatMap(s->Arrays.stream(s.split("\n"))).forEach(s->System.out.println(s));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
